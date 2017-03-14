1ab217a4252630ce34906782997afa244f7a2cb9
dml <dml1630@163.com>
2017-03-14 16:13:17 +0800

    Swift波浪动画
    CAShapeLayer， UIBezierPath ， 结合余弦正弦函数完成简单的波浪效果

---
+++ RomWavesCustom/RomWavesCustom/RomWavesCustom.swift
@@ -0,0 +1,162 @@
+//
+//  RomWavesCustom.swift
+//  RomWavesCustom
+//
+//  Created by 戴明亮 on 17/3/8.
+//  Copyright © 2017年 戴明亮. All rights reserved.
+//
+/**
+ y = Asin(wx + φ) + C
+ A 振幅调整浪的高度
+ w 周期调整浪的数量
+ φ 初相调整波浪的位移
+ C 波浪的纵向位置
+ 
+ */
+import UIKit
+
+class RomWavesCustom: UIView {
+
+    
+    // ************************************
+    lazy var firstWavesLayer: CAShapeLayer = CAShapeLayer.init()
+    lazy  var wavesDisplayLink: CADisplayLink = CADisplayLink.init(target: self, selector: #selector(startwaves))
+    // 懒加载的复杂的写法
+    var secondWavesLayer: CAShapeLayer  = { () -> (CAShapeLayer) in
+        let layer = CAShapeLayer.init()
+        return layer
+    }()
+    
+    var wavesA_First: CGFloat = 0 // 波浪振幅
+    var wavesW_First: CGFloat = 0 // 波浪周期
+    var offsetX_First: CGFloat = 0 // 偏移
+    var wavesSpeed_Frist: CGFloat = 0 // 波浪速度
+    var wavesWidth_First: CGFloat = 0 // 波浪宽度
+    var wavesCurrentY_Frist: CGFloat = 0 // 当前波浪的高度（起始高度）
+    
+    
+    var wavesA_Second: Double = 0
+    var wavesW_Seocnd: Double = 0
+    var offsetX_Second: Double = 0
+    var wavesSpeed_Second: Double = 0
+    
+    var currentYOffset: ((_ offset: CGFloat ) -> ())?
+//    var currentYOffset: (_ offset: CGFloat ) -> ()
+    
+    // ***********************************
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+        setupSubViews()
+    }
+    
+    @objc private func startwaves() {
+        
+        offsetX_First += wavesSpeed_Frist
+        offsetX_Second += wavesSpeed_Second
+        setCurrentFirstWavesLayerPath()
+        setCurrentSecondWavesLayerPath()
+    }
+    
+    
+    /// 绘制第一个波浪
+    func setCurrentFirstWavesLayerPath(){
+        
+       let path = UIBezierPath.init()
+        
+       path.move(to: CGPoint.init(x: 0, y: wavesCurrentY_Frist))
+        for i in 0...NSInteger(wavesWidth_First) {
+            let y = wavesA_First * sin(wavesW_First * CGFloat(i) + offsetX_First) + wavesCurrentY_Frist
+            path.addLine(to: CGPoint.init(x: CGFloat(i), y: y))
+            
+            guard let currentYOffsetBlock = currentYOffset else {
+                return
+            }
+            currentYOffsetBlock(CGFloat(y))
+            
+            
+            
+        }
+        path.addLine(to: CGPoint.init(x: wavesWidth_First, y: self.frame.size.height))
+        
+       path.addLine(to: CGPoint.init(x: 0, y: self.frame.size.height))
+        
+        path.close()
+        
+        firstWavesLayer.path = path.cgPath
+ 
+        
+    }
+    
+    
+    /// 绘制第二个波浪
+    func setCurrentSecondWavesLayerPath(){
+        
+        let path = UIBezierPath.init()
+        path.move(to: CGPoint.init(x: 0, y: wavesCurrentY_Frist))
+        
+        
+        for i in 0...Int(wavesWidth_First) {
+            
+            let y = wavesA_Second * cos( wavesW_Seocnd * Double(i) +  offsetX_Second * M_PI/2.0) + Double(wavesCurrentY_Frist)
+            
+            path.addLine(to: CGPoint.init(x: Double(i), y: y))
+            
+           
+        }
+        
+        path.addLine(to: CGPoint.init(x: Double(wavesWidth_First), y: Double(self.frame.size.height)))
+        path.addLine(to: CGPoint.init(x: Double(0), y: Double(self.frame.size.height)))
+        path.close()
+    
+        secondWavesLayer.path = path.cgPath
+        
+       
+    }
+    
+
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+
+// MARK: - lazy
+ internal extension RomWavesCustom {
+
+    // 布局子控件
+     internal func setupSubViews() {
+        backgroundColor = UIColor.clear
+        layer.masksToBounds = true
+        
+        wavesWidth_First = CGFloat(self.frame.size.width)
+        wavesCurrentY_Frist = CGFloat(self.frame.size.height * 0.5)
+        
+        wavesSpeed_Frist = 0.02;
+        wavesA_First = 12;
+        wavesW_First = 0.5/30.0
+        
+        wavesSpeed_Second = 0.03;
+        wavesA_Second = 13
+        wavesW_Seocnd = 0.5/30.0
+        
+        
+        
+        firstWavesLayer.fillColor = UIColor.white.cgColor
+        secondWavesLayer.fillColor = UIColor.white.withAlphaComponent(0.5).cgColor
+        self.layer.addSublayer(firstWavesLayer)
+        self.layer.addSublayer(secondWavesLayer)
+        // 将定时器添加到当前运行循环中
+        wavesDisplayLink.add(to: RunLoop.current, forMode: .commonModes)
+
+    }
+   
+ 
+    
+}
---
+++ RomWavesCustom/RomWavesCustom/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  RomWavesCustom
+//
+//  Created by 戴明亮 on 17/3/8.
+//  Copyright © 2017年 戴明亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var tableView: UITableView? = { () -> (UITableView) in
+        let tab = UITableView.init(frame: UIScreen.main.bounds, style: .plain)
+       
+        return tab
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        guard let tableView = tableView else {
+            return
+        }
+        view.addSubview(tableView)
+    }
+    
+    //
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         var cell = tableView.dequeueReusableCell(withIdentifier: "cellId")
+        
+        if cell == nil {
+            cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "cellId")
+        }
+
+        return cell!
+    }
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        var headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId")
+        
+        
+        if headerView == nil {
+            headerView = UITableViewHeaderFooterView.init(reuseIdentifier: "headerId")
+        
+            let  waves  = RomWavesCustom.init(frame: CGRect.init(x: 0, y: 120, width: UIScreen.main.bounds.width, height: 80))
+            headerView?.addSubview(waves)
+            
+            let imgv = UIImageView.init()
+            imgv.image = UIImage.init(named: "swiftIcon")
+            let size = CGSize.init(width: 50, height: 50)
+                //imgv.image?.size ?? CGSize.init(width: 50, height: 50);
+            imgv.center = CGPoint.init(x: UIScreen.main.bounds.size.width * 0.5, y: 145 - size.height * 0.5)
+            imgv.bounds = CGRect.init(x: 0, y: 0, width: size.width, height: size.height)
+            imgv.layer.cornerRadius = size.width * 0.5
+            imgv.clipsToBounds = true
+            let originY = imgv.frame.origin.y
+            waves.currentYOffset = {  (offset) -> () in
+                var rect = imgv.frame
+                var inset = offset
+                inset -= 25
+     
+                rect.origin.y = (originY - 25) + inset
+              
+                imgv.frame =  rect
+            }
+            
+            headerView?.addSubview(imgv)
+            
+        }
+
+        headerView?.layer.backgroundColor = UIColor.init(colorLiteralRed: 245/255.0, green: 68/255.0, blue: 4/255.0, alpha: 0.95).cgColor
+       
+        return headerView;
+
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 200;
+    }
+    
+}
